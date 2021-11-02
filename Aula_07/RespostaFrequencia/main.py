be8c0dcc8e9ecf4a30b8d33371187b6c8e8a1fed
Gustavo da Silva Mafra <49033933+GustavoSMafra@users.noreply.github.com>
2021-11-01 21:26:46 -0300

    Add files via upload

--- Aula_07/RespostaFrequencia/main.py
+++ Aula_07/RespostaFrequencia/main.py
@@ -1,68 +1,71 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import scipy
+import PySimpleGUI as sg
 from scipy import signal
 
-passo = np.pi / 1000
-w = np.arange(0, np.pi, passo)
-L = 8
-Fs = 8000
 
-opc = input("Qual exercício: ")
+sg.change_look_and_feel('DarkGrey2')
 
-if opc == "Exemplo":
-    num = np.sin(w * L / 2)
-    den = np.sin(w / 2)
+layout = [
+    [sg.Text("Numerador:    "), sg.Input(size=(20, 0), key = 'Num')],
+    [sg.Text("Denominador: "), sg.Input(size=(20, 0), key = 'Den')],
+    [sg.Listbox(['Exemplo', '4-A', '4-B', '4-C', 'Passa baixa', 'Passa alta' , 'Valores alocados'], size=(20,7), key = "List")],
+    [sg.Button("Calcular")]
+]
 
-elif opc == "4-A":
-    #Questão 4-A
-    num = 3*(np.exp(1j*w) - 1.2)
-    den = (np.exp(1j*w)-0.5)*(np.exp(1j*w)-0.9)
+janela = sg.Window('Respota em frequência', size=(400, 300), element_justification='l').layout(layout)
+event, values = janela.read()
 
-elif opc == "4-B":
-    #Questão 4-B
-    num = np.exp(1j*w)
-    den = (np.exp(1j*w) - 0.9)*(np.exp(1j*w) - 1.2)
+while True:
+    if event == sg.WINDOW_CLOSED:
+        break
+    elif event == "Calcular":
+        opc = (values["List"])
+        Fs = 8000
 
-elif opc == "4-C":
-    #Questão 4-C
+        if opc[0] == "Exemplo":
+            L = 8
+            num = np.zeros(L)
+            num[0:] = 1 / L
+            #num = [0.1, 0.2, 0.4, 0.2, 0.1]
+            #den = 1
 
-    num = (np.exp(1j*w) + 0.9)
-    den = ((np.exp(1j*w)**2) + np.exp(1j*w)  + 0.41)
+        elif opc[0] == "4-A":
+            #Questão 4-A
+            num = [0, 3, -3.6]
+            den = [1, -1.4, 0.45]
 
-temp = num/den
+        elif opc[0] == "4-B":
+            #Questão 4-B
+            num = [0, 1, 0]
+            den = [1, -2.1, 1.08]
 
-X = (1 / L) * (abs(temp))
-plt.subplot(3, 1, 1)
-plt.plot(w, X)
-plt.xlabel('Frquência')
-plt.title('Frequêncua Rad')
-plt.grid()
+        elif opc[0] == "4-C":
+            #Questão 4-C
+            num = [0, 1, 0.9]
+            den = [1, 1, 0.41]
 
+        elif opc[0] == "Passa baixa":
+            #Questão 4-C
+            num = [0, 6280, 6280]
+            den = [0, 22280, -9720]
 
-F_Hz = (w / np.pi) * (Fs / 2)
-plt.subplot(3, 1, 2)
-plt.plot(F_Hz, X)
-plt.xlabel('Frequência')
-plt.title('Frequência em Hz')
-plt.grid()
+        elif opc[0] == "Passa alta":
+            #Questão 4-C
+            num = [0, 16000, -16000]
+            den = [0, 22280, -9720]
 
-X_Db = 20 * np.log10(X)
-plt.subplot(3, 1, 3)
-plt.plot(F_Hz, X_Db)
-plt.ylabel('Atenuação DB')
-plt.xlabel('Frequência')
-plt.title('Frequência em Hz')
-plt.grid()
+        elif opc[0] == "Valores alocados":
+            #Qualquer expressão colocada
+            num = list(map(float, values["Num"].split(',')))
+            den = list(map(float, values["Den"].split(',')))
+            print(num)
+            print(den)
 
-plt.axis([0, 4000, - 70, 0])
-num = np.zeros(L)
-num[0:] = 1 / L
-den = [1]
-[w, H] = signal.freqz(num, den, Fs)
-Freq = Fs * w / (2*np.pi)
-plt.plot(Freq * Fs / (2 * np.pi), 20 * np.log10(abs(H)))
-plt.title('Magnitude da resposta em frequencia')
+        w, H = signal.freqz(num, den, Fs)
+        Freq = Fs*w/(2*np.pi)
+        plt.plot(Freq, 20*np.log10(abs(H)))
+        plt.title('Magnitude da resposta em frequencia')
 
-plt.tight_layout()
-plt.show()
+        plt.show()
+        event, values = janela.read()
