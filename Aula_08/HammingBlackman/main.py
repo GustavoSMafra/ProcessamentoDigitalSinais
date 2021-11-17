8bb6cf0502c6be7c99e9ba1f5e94db22bf1d8dcc
Gustavo da Silva Mafra <49033933+GustavoSMafra@users.noreply.github.com>
2021-11-17 14:29:25 -0300

    Update main.py

--- Aula_08/HammingBlackman/main.py
+++ Aula_08/HammingBlackman/main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy import signal
 
 M = int(input("M: "))
 
@@ -34,3 +35,28 @@ plt.grid()
 
 plt.tight_layout()
 plt.show()
+
+fs = 8000
+
+plt.subplot(2, 2, 1)
+plt.title("Hamming")
+[w1, L] = signal.freqz(H, 1, fs)
+plt.plot(w1*fs/(2*np.pi), 20*np.log10(abs(L)))
+plt.grid()
+
+plt.subplot(2, 2, 2)
+plt.title("Blackman")
+[w2, J] = signal.freqz(B, 1, fs)
+plt.plot(w2*fs/(2*np.pi), 20*np.log10(abs(J)))
+plt.grid()
+
+plt.subplot(2,2,3)
+plt.title("Black x Hamming")
+plt.plot(w1*fs/(2*np.pi), 20*np.log10(abs(L)),  'r')
+plt.plot(w2*fs/(2*np.pi), 20*np.log10(abs(J)),  'b')
+plt.legend(['Hamming', 'Blackman'])
+plt.grid()
+
+plt.tight_layout()
+plt.show()
+
