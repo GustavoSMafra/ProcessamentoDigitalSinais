cfe4146eef5c1bdcc39496d3e31a1ffd1118fbdf
Gustavo da Silva Mafra <49033933+GustavoSMafra@users.noreply.github.com>
2021-12-13 19:10:22 -0300

    Add files via upload

--- M3/Equalizador_DSP/Equalizador_DSP.c
+++ M3/Equalizador_DSP/Equalizador_DSP.c
@@ -11,12 +11,10 @@ float coefPB[NSAMPLES] = {
 float coefPA[NSAMPLES] = {
 	#include "Coef_PA.dat" // Coeficiente Passa Alta 
 };
-float coefPF[NSAMPLES] = {
+float coefBP[NSAMPLES] = {
 	#include "Coef_BP.dat" // Coeficiente Passa Faixa 
 };
 
-
-extern short proc_alg( float *, short *, int);
 
 int main(int argc,char *argv[])
 {
@@ -24,7 +22,7 @@ int main(int argc,char *argv[])
 	FILE *fin,*fout;
 	
 	short entrada, saida;
-	float saidaPB, saidaPA, saidaBP;
+	float saidaPB, saidaPA, saidaBP, saidaT; 
 
   	short Vet_entr[NSAMPLES]= {0x0};
   	
@@ -38,7 +36,7 @@ int main(int argc,char *argv[])
 	printf("\n");
 	
 	
-	fin = fopen("..\\sweep_100_2k.pcm","rb");
+	fin = fopen("..\\sweep_100_3k4.pcm","rb");
     if ((fin)==NULL){
     	printf("\nErro: nao abriu o arquivo de Entrada\n");
     	return 0;
@@ -51,33 +49,44 @@ int main(int argc,char *argv[])
   	
   	CYCLES_INIT(stats);
   		
-  printf("Processando ...\n ");
+	printf("Processando ...\n ");
+	  
+	while (fread(&entrada,sizeof(short),1,fin) == 1){
+	  	saidaPB = 0;
+		saidaBP = 0;
+		saidaPA = 0;
+  		
+		CYCLES_START(stats);
+		
+		Vet_entr[0] = entrada;
 	
-  while (fread(&entrada,sizeof(short),1,fin) == 1){
-  		
-  		Vet_entr[0] = entrada;
-  		
-		CYCLES_START(stats);	
+		int n = 0;
+        for(n = 0; n < NSAMPLES; n++){
+            saidaPB += coefPB[n] * Vet_entr[n] * PB_input;
+            saidaBP += coefBP[n] * Vet_entr[n] * BP_input;
+            saidaPA += coefPA[n] * Vet_entr[n] * PA_input;
+
+            saidaT = saidaPB + saidaBP + saidaPA;
+        }
+        for(n = NSAMPLES-1; n>0; n--){
+            Vet_entr[n] = Vet_entr[n-1];
+        }	
 		
-		saidaPB = proc_alg( coefPB, Vet_entr, NSAMPLES);
-		saidaPA = proc_alg( coefPA, Vet_entr, NSAMPLES);
-		saidaBP = proc_alg( coefPF, Vet_entr, NSAMPLES);
-		
-		saida = (saidaPB * PB_input) + (saidaPA * PA_input) + (saidaBP * BP_input);
+		saida = saidaT;
 
 		CYCLES_STOP(stats);
 		fwrite(&saida,sizeof(short),1,fout);	
 			
 	}
 
-    printf("terminado!\n");
+	printf("terminado!\n");
 		
     
 	CYCLES_PRINT(stats);
 	fclose(fin);
 	fclose(fout);
 		
-    return 0;
+	return 0;
 }
 
 
